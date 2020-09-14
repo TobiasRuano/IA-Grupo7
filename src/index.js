d626af65b8d41c28c064c9787f8b800dc5f69825
Hugo Andres Vazquez <hugo@nubeit.net>
2020-09-13 21:29:39 -0300

    Cambio de rutas

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ import "assets/scss/material-kit-react.scss?v=1.9.0";
 
 // pages for this product
 import Components from "views/Components/Components.js";
-import LandingPage from "views/LandingPage/LandingPage.js";
-import ProfilePage from "views/ProfilePage/ProfilePage.js";
+import HistoriasClinicas from "views/LandingPage/HistoriasClinicas.js";
+import Turnos from "views/ProfilePage/Turnos.js";
 import LoginPage from "views/LoginPage/LoginPage.js";
 
 var hist = createBrowserHistory();
@@ -16,8 +16,8 @@ var hist = createBrowserHistory();
 ReactDOM.render(
   <Router history={hist}>
     <Switch>
-      <Route path="/landing-page" component={LandingPage} />
-      <Route path="/profile-page" component={ProfilePage} />
+      <Route path="/historias-clinicas" component={HistoriasClinicas} />
+      <Route path="/Turnos" component={Turnos} />
       <Route path="/login-page" component={LoginPage} />
       <Route path="/" component={Components} />
     </Switch>
