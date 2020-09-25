69ff82cd8dff622a983d6de8b4f63d2fad103e86
Tobias Ruano <ruano.t10@gmail.com>
2020-09-25 14:40:23 -0300

    Le agrege un fondo a la pagina de turnos

--- src/views/Turnos/TurnosPage.js
+++ src/views/Turnos/TurnosPage.js
@@ -28,6 +28,8 @@ import CustomInput from "components/CustomInput/CustomInput.js";
 import TablaTurnos from "components/TablaTurnos/TablaTurnos.js";
 
 import styles from "assets/jss/material-kit-react/views/loginPage.js";
+import image from "assets/img/bg7.jpg";
+import { Container } from "@material-ui/core";
 
 const useStyles = makeStyles(styles);
 const dashboardRoutes = [];
@@ -36,34 +38,45 @@ export default function TurnosPage(props) {
   const [cardAnimaton, setCardAnimation] = React.useState("cardHidden");
   const [age, setAge] = React.useState('');
   const patientName = "Tobias";
+  const classes = useStyles();
+  const { ...rest } = props;
+
   const handleChange = (event) => {
     setAge(event.target.value);
   };
   setTimeout(function() {
     setCardAnimation("");
   }, 700);
-  const classes = useStyles();
-  const { ...rest } = props;
+  
   return (
       <div>
-          <Header
-            color="transparent"
-            routes={dashboardRoutes}
-            brand="Home"
-            rightLinks={<HeaderLinks />}
-            fixed
-            changeColorOnScroll={{
-            height: 400,
-            color: "white"
-            }}
-            {...rest}
-            />
+        <Header
+          color="transparent"
+          routes={dashboardRoutes}
+          brand="Inicio"
+          rightLinks={<HeaderLinks />}
+          fixed
+          changeColorOnScroll={{
+          height: 400,
+          color: "white"
+          }}
+          {...rest}
+        />
+        <div
+          className={classes.pageHeader}
+          style={{
+            backgroundImage: "url(" + image + ")",
+            backgroundSize: "cover",
+            backgroundPosition: "top center"
+          }}
+      >
         <div className={classNames(classes.main, classes.mainRaised)}>
             <div className={classes.container}>
               <h1>Bienvenido {patientName}</h1>
                 <TablaTurnos></TablaTurnos>
             </div>
         </div>
+      </div>
       </div>
   );
 }
