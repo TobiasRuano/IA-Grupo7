68e13cd813a84d53be4c049ceaa81854e76f1339
Tobias Ruano <ruano.t10@gmail.com>
2020-10-04 18:21:59 -0300

    Tabla turnos agregada al UserProfile

--- src/views/UserProfile/SectionDownload.js
+++ src/views/UserProfile/SectionDownload.js
@@ -35,46 +35,9 @@ export default function SectionDownload() {
             </Button>
           </GridItem>
         </GridContainer>
-        <br />
-        <br />
-        <GridContainer className={classes.textCenter} justify="center">
-          <GridItem xs={12} sm={12} md={8}>
-            <h2>Want more?</h2>
-            <h4>
-              We{"'"}ve launched{" "}
-              <a
-                href="https://www.creative-tim.com/product/material-kit-pro-react?ref=mkr-download-section"
-                target="_blank"
-              >
-                Material Kit PRO React{" "}
-              </a>
-              .It has a huge number of components, sections and example pages.
-              Start Your Development With A Badass Material-UI nspired by
-              Material Design.
-            </h4>
-          </GridItem>
-          <GridItem xs={12} sm={8} md={6}>
-            <Button
-              color="rose"
-              size="lg"
-              href="https://www.creative-tim.com/product/material-kit-pro-react?ref=mkr-download-section"
-              target="_blank"
-            >
-              Material Kit PRO
-            </Button>
-            <Button
-              color="rose"
-              size="lg"
-              href="https://www.creative-tim.com/product/material-dashboard-pro-react?ref=mkr-download-section"
-              target="_blank"
-            >
-              Material Dashboard PRO
-            </Button>
-          </GridItem>
-        </GridContainer>
         <div className={classes.textCenter + " " + classes.sharingArea}>
           <GridContainer justify="center">
-            <h3>Desea Eliminar su Cuenta?</h3>
+            <h2>Desea Eliminar su Cuenta?</h2>
           </GridContainer>
           <Button
             color="danger"
--- src/views/UserProfile/UserProfile.js
+++ src/views/UserProfile/UserProfile.js
@@ -2,9 +2,9 @@ import React from "react";
 // @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
 //import TextField from '@material-ui/core/TextField';
-import FormControl from '@material-ui/core/FormControl';
-import Input from '@material-ui/core/Input';
-import InputLabel from '@material-ui/core/InputLabel';
+import FormControl from "@material-ui/core/FormControl";
+import Input from "@material-ui/core/Input";
+import InputLabel from "@material-ui/core/InputLabel";
 
 // core components
 import GridItem from "components/Grid/GridItem.js";
@@ -19,6 +19,7 @@ import Footer from "components/Footer/Footer.js";
 import Parallax from "components/Parallax/Parallax.js";
 import classNames from "classnames";
 import profile from "assets/img/faces/christian.jpg";
+import TablaTurnos from "components/TablaTurnos/TablaTurnos.js";
 
 import CardBody from "components/Card/CardBody.js";
 
@@ -47,12 +48,11 @@ import styles from "assets/jss/material-kit-react/views/profilePage.js";
 
 const useStyles = makeStyles(styles);
 
-
 const useStylesTheme = makeStyles((theme) => ({
   root: {
-    '& > *': {
+    "& > *": {
       margin: theme.spacing(1),
-      width: '25ch',
+      width: "25ch",
     },
   },
 }));
@@ -62,74 +62,67 @@ export default function UserProfile(props) {
   const classes1 = useStylesTheme();
   const { ...rest } = props;
   //datos paciente
-  const [name, setName] = React.useState('Tobias');
-  const [surname, setSurname] = React.useState('Ruano');
-  const [dni, setDni] = React.useState('00000001');
-  const [sexo, setSexo] = React.useState('Masculino');
-  const [fechanac, setFechanac] = React.useState('06/06/97');
-  const [email, setEmail] = React.useState('truano@uade.edu.ar');
-  const [domicilio, setDom] = React.useState('Lima 775');
-  const [telefono, setTel] = React.useState('1515251515');
-  
+  const [name, setName] = React.useState("Tobias");
+  const [surname, setSurname] = React.useState("Ruano");
+  const [dni, setDni] = React.useState("00000001");
+  const [sexo, setSexo] = React.useState("Masculino");
+  const [fechanac, setFechanac] = React.useState("06/06/97");
+  const [email, setEmail] = React.useState("truano@uade.edu.ar");
+  const [domicilio, setDom] = React.useState("Lima 775");
+  const [telefono, setTel] = React.useState("1515251515");
+
   //datos medicos
-  const [gruposang, setGrupoSang] = React.useState('A+');
-  const [cardiaco, setCardiaco] = React.useState('NO');
-  const [diabetes, setDiabetes] = React.useState('NO');
-  const [hiperten, setHiperten] = React.useState('NO');
-  const [alergias, setAlergias] = React.useState('NINGUNA');
- 
-  
+  const [gruposang, setGrupoSang] = React.useState("A+");
+  const [cardiaco, setCardiaco] = React.useState("NO");
+  const [diabetes, setDiabetes] = React.useState("NO");
+  const [hiperten, setHiperten] = React.useState("NO");
+  const [alergias, setAlergias] = React.useState("NINGUNA");
 
-
- // const [Actualizar, setActualizar] = React.useState(false);
-
-
+  // const [Actualizar, setActualizar] = React.useState(false);
 
   const handleChangeGrupoSang = (event) => {
     setGrupoSang(event.target.value);
-    
-  }
+  };
   const handleChangeCardiaco = (event) => {
     setCardiaco(event.target.value);
-  }
+  };
   const handleChangeDiabetes = (event) => {
     setDiabetes(event.target.value);
-  }
+  };
   const handleChangeHiperten = (event) => {
     setHiperten(event.target.value);
-  }
+  };
   const handleChangeAlergias = (event) => {
     setAlergias(!event.target.value);
-  }
+  };
 
   const handleChangeName = (event) => {
     setName(event.target.value);
-  }
+  };
   const handleChangeDni = (event) => {
     setDni(event.target.value);
-  }
+  };
   const handleChangeEmail = (event) => {
     setEmail(event.target.value);
-  }
+  };
   const handleChangeFechanac = (event) => {
     setFechanac(event.target.value);
-  }
+  };
   const handleChangeSexo = (event) => {
     setSexo(event.target.value);
-  }
+  };
   const handleChangeSurname = (event) => {
     setSurname(event.target.value);
-  }
+  };
   const handleChangeTel = (event) => {
     setTel(event.target.value);
-  }
+  };
   const handleChangeDom = (event) => {
     setDom(event.target.value);
-  }
+  };
 
-  function deleteAccount() {
-  }
-  
+  function deleteAccount() {}
+
   const imageClasses = classNames(
     classes.imgRaised,
     classes.imgRoundedCircle,
@@ -145,7 +138,7 @@ export default function UserProfile(props) {
         fixed
         changeColorOnScroll={{
           height: 200,
-          color: "white"
+          color: "white",
         }}
         {...rest}
       />
@@ -167,78 +160,145 @@ export default function UserProfile(props) {
             </GridContainer>
             <GridContainer justify="center">
               <GridItem xs={12} sm={12} md={8} className={classes.navWrapper}>
-              <Card>
-          <CardHeader color="primary">
-            <h4 className={classes.cardTitleWhite}>Datos Generales </h4>
-          </CardHeader>
-          <CardBody>
-            <GridContainer>
-              <GridItem xs={12} sm={12} md={4}>
-              <form className={classes1.root} nonValidate autoComplete="off">
-                 <FormControl disabled>
-                  <InputLabel htmlFor="component-surname">Dni</InputLabel>
-                  <Input id="component-disabled" value={dni} onChange={handleChangeDni} />
-                </FormControl>
-              </form>
+                <Card>
+                  <CardHeader color="primary">
+                    <h4 className={classes.cardTitleWhite}>Datos Generales </h4>
+                  </CardHeader>
+                  <CardBody>
+                    <GridContainer>
+                      <GridItem xs={12} sm={12} md={4}>
+                        <form
+                          className={classes1.root}
+                          nonValidate
+                          autoComplete="off"
+                        >
+                          <FormControl disabled>
+                            <InputLabel htmlFor="component-surname">
+                              Dni
+                            </InputLabel>
+                            <Input
+                              id="component-disabled"
+                              value={dni}
+                              onChange={handleChangeDni}
+                            />
+                          </FormControl>
+                        </form>
+                      </GridItem>
+                    </GridContainer>
+
+                    <GridContainer>
+                      <GridItem xs={12} sm={12} md={4}>
+                        <form
+                          className={classes1.root}
+                          nonValidate
+                          autoComplete="off"
+                        >
+                          <FormControl disabled>
+                            <InputLabel htmlFor="component-sexo">
+                              Sexo
+                            </InputLabel>
+                            <Input
+                              id="component-disabled"
+                              value={sexo}
+                              onChange={handleChangeSexo}
+                            />
+                          </FormControl>
+                        </form>
+                      </GridItem>
+
+                      <GridItem xs={12} sm={12} md={4}>
+                        <form
+                          className={classes1.root}
+                          nonValidate
+                          autoComplete="off"
+                        >
+                          <FormControl disabled>
+                            <InputLabel htmlFor="component-fechanac">
+                              Fecha de Nacimiento
+                            </InputLabel>
+                            <Input
+                              id="component-disabled"
+                              value={fechanac}
+                              onChange={handleChangeFechanac}
+                            />
+                          </FormControl>
+                        </form>
+                      </GridItem>
+
+                      <GridItem xs={12} sm={12} md={4}>
+                        <form
+                          className={classes1.root}
+                          nonValidate
+                          autoComplete="off"
+                        >
+                          <FormControl disabled>
+                            <InputLabel htmlFor="component-surname">
+                              Telefono
+                            </InputLabel>
+                            <Input
+                              id="component-disabled"
+                              value={telefono}
+                              onChange={handleChangeTel}
+                            />
+                          </FormControl>
+                        </form>
+                      </GridItem>
+                    </GridContainer>
+
+                    <GridContainer>
+                      <GridItem xs={12} sm={12} md={4}>
+                        <form
+                          className={classes1.root}
+                          nonValidate
+                          autoComplete="off"
+                        >
+                          <FormControl disabled>
+                            <InputLabel htmlFor="component-domicilio">
+                              Domicilio
+                            </InputLabel>
+                            <Input
+                              id="component-disabled"
+                              value={domicilio}
+                              onChange={handleChangeDom}
+                            />
+                          </FormControl>
+                        </form>
+                      </GridItem>
+
+                      <GridItem xs={12} sm={12} md={4}>
+                        <form
+                          className={classes1.root}
+                          nonValidate
+                          autoComplete="off"
+                        >
+                          <FormControl disabled>
+                            <InputLabel htmlFor="component-email">
+                              E-mail
+                            </InputLabel>
+                            <Input
+                              id="component-disabled"
+                              value={email}
+                              onChange={handleChangeEmail}
+                            />
+                          </FormControl>
+                        </form>
+                      </GridItem>
+                    </GridContainer>
+                  </CardBody>
+                </Card>
               </GridItem>
             </GridContainer>
-            
-            <GridContainer>
-              <GridItem xs={12} sm={12} md={4}>
-              <form className={classes1.root} nonValidate autoComplete="off">
-                 <FormControl disabled>
-                  <InputLabel htmlFor="component-sexo">Sexo</InputLabel>
-                  <Input id="component-disabled" value={sexo} onChange={handleChangeSexo} />
-                </FormControl>
-              </form>
-              </GridItem>
 
-              <GridItem xs={12} sm={12} md={4}>
-              <form className={classes1.root} nonValidate autoComplete="off">
-                 <FormControl disabled>
-                  <InputLabel htmlFor="component-fechanac">Fecha de Nacimiento</InputLabel>
-                  <Input id="component-disabled" value={fechanac} onChange={handleChangeFechanac} />
-                </FormControl>
-              </form>
-              </GridItem>
-
-              <GridItem xs={12} sm={12} md={4}>
-              <form className={classes1.root} nonValidate autoComplete="off">
-                 <FormControl disabled>
-                  <InputLabel htmlFor="component-surname">Telefono</InputLabel>
-                  <Input id="component-disabled" value={telefono} onChange={handleChangeTel} />
-                </FormControl>
-              </form>
-              </GridItem>
-
-            </GridContainer>
-
-            <GridContainer>
-              <GridItem xs={12} sm={12} md={4}>
-                  
-              <form className={classes1.root} nonValidate autoComplete="off">
-                 <FormControl disabled>
-                  <InputLabel htmlFor="component-domicilio">Domicilio</InputLabel>
-                  <Input id="component-disabled" value={domicilio} onChange={handleChangeDom} />
-                </FormControl>
-              </form>
-                
-              </GridItem>
-
-              <GridItem xs={12} sm={12} md={4}>
-              <form className={classes1.root} nonValidate autoComplete="off">
-                 <FormControl disabled>
-                  <InputLabel htmlFor="component-email">E-mail</InputLabel>
-                  <Input id="component-disabled" value={email} onChange={handleChangeEmail} />
-                </FormControl>
-              </form>
-              </GridItem>
-
-            </GridContainer>
-            
-          </CardBody>
-        </Card>
-              </GridItem>
+            <GridContainer justify="center">
+            <Card>
+              <CardHeader color="primary">
+                <h4 className={classes.cardTitleWhite}>Mis Turnos </h4>
+                <p>Aqui puede manejar sus turnos actuales</p>
+              </CardHeader>
+              <CardBody >
+                <TablaTurnos></TablaTurnos>
+              </CardBody>
+            </Card>
             </GridContainer>
             <SectionDownload />
           </div>
